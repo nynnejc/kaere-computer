034e3b82ea55e7a142e0e418d8188d441792d7da
nynnejc <nynnejc@gmail.com>
2024-12-10 10:52:59 +0100

    next config

--- next.config.js
+++ next.config.js
@@ -1,11 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
+  output: "export", // Enables static export mode
+  distDir: "public", // Specifies "public" as the output directory
   reactStrictMode: true,
-  swcMinify: false,
+  swcMinify: false, // Keeps SWC minification disabled
   images: {
-    unoptimized: true
-},
+    unoptimized: true, // Ensures images work in static export mode
+  },
 };
 
 module.exports = nextConfig;
